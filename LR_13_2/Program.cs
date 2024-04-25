687b500816537b0c7e2bdcb3600bc5fc3ae2a427
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-04-26 00:33:35 +0300

    Добавил лабораторные №13_1 и 13_2

---
+++ LR_13_2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LR_13_2
+{
+    internal class Program
+    {
+        static double f (double x, double a)
+        {
+            double y;
+            if (x < a) y = 0;
+            else if (x > a) y = (x - a)/(x + a);
+            else y = 1;
+            return y;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начало отрезка a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конец отрезка b:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите шаг h:");
+            double h = double.Parse(Console.ReadLine());
+
+            for (double i = a; i <= b; i += h) {
+                Console.WriteLine($"x = {i}, f(x) = {f(i, a)}");
+            }
+        }
+    }
+}
