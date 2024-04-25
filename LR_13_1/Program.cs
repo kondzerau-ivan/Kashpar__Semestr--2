60fdd764015404bfc4255776219990401510b50a
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-04-26 00:34:35 +0300

    Добавил лабораторную 13_1

---
+++ LR_13_1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LR_13_1
+{
+    internal class Program
+    {
+
+        static void f(double x1, double y1, double x2, double y2)
+        {
+            double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine(result);
+        }
+
+        static void t(double a, double b, double c)
+        {
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                Console.WriteLine($"Треугольник со сторонами а = {a}, b = {b}, c = {c} - Существует!");
+            } else
+            {
+                Console.WriteLine($"Треугольник со сторонами а = {a}, b = {b}, c = {c} - Не существует!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисление длины отрезка по заданным координатам:");
+            f(1, 3, 2, 5);
+
+            Console.WriteLine("Проверка существования треугольника по заданным сторонам:");
+            t(2, 3, 4);
+        }
+    }
+}
